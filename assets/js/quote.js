bd9f63ed1e49e04f1307735a20238bf33e0cab0d
ACLTearr <burroughskaelen@gmail.com>
2021-01-15 17:13:33 -0500

    Finished Quote form js to show and hide forms correctly

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -1,4 +1,26 @@
-$("#building-type").on("change", function() {
-    $("#" + $(this).val()).show();
-    $("#" + $(this).val()).hide();
-})
+$('#building-type').on('change',function(){
+    if( $(this).val()==="residential"){
+        $("#residential").show()
+        $("#commercial").hide()
+        $("#corporate").hide()
+        $("#hybrid").hide()
+    }
+    else if( $(this).val()==="commercial"){
+        $("#commercial").show()
+        $("#residential").hide()
+        $("#corporate").hide()
+        $("#hybrid").hide()
+    }
+    else if( $(this).val()==="corporate"){
+        $("#corporate").show()
+        $("#commercial").hide()
+        $("#residential").hide()
+        $("#hybrid").hide()
+    }
+    else if( $(this).val()==="hybrid"){
+        $("#hybrid").show()
+        $("#commercial").hide()
+        $("#corporate").hide()
+        $("#residential").hide()
+    }
+});
