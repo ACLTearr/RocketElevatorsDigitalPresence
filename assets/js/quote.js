9f820154d1020bf8d7aca1e2e6f7ea47535c9f99
ACLTearr <burroughskaelen@gmail.com>
2021-01-18 15:16:18 -0500

    Finished all calculations (roughly)

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     });
 
   //Set default tier cost
-    $("#elevator-unit-price").val("$" + $("input[type='radio'][name='tier-selected']:checked").val());
+  $("#elevator-unit-price").val("$" + $("input[type='radio'][name='tier-selected']:checked").val());
   
   //Tier Selection Cost Setting (Price per unit output)
   $("input[type='radio'][name='tier-selected']").on("change", function() {
@@ -37,11 +37,84 @@ $(document).ready(function() {
     //Setting output fields
     $("#elevator-amount").val(totalElevators + " Elevators");
     $("#elevator-total-price").val("$" + elevatorCost);
-    $("#installation-fees").val("$" + elevatorCost * installationFees);
     $("#final-price").val("$" + finalPrice);
+    $("#installation-fees").val("$" + elevatorCost * installationFees).toFixed(2);
   });
 
-  /* Moving around comment (delete before final push)
-  
-  */
+  //Commercial Form Calcs
+  $('#commercial-elevator-cages').on('keyup', function() {
+    //Commercial Form Variables
+    if ($("input[type='radio'][name='tier-selected']:checked").val() === "7565") {
+      var installationFees = 0.1
+    } else if ($("input[type='radio'][name='tier-selected']:checked").val() === "12345") {
+      var installationFees = 0.13
+    } else if ($("input[type='radio'][name='tier-selected']:checked").val() === "15400") {
+      var installationFees = 0.16
+    };
+    var commercialElevatorCages = $("#commercial-elevator-cages").val();
+    //Calculations
+    var elevatorCost = commercialElevatorCages * $("input[type='radio'][name='tier-selected']:checked").val();
+    var finalPrice = elevatorCost + (elevatorCost * installationFees);
+    //Setting output fields
+    $("#elevator-amount").val(commercialElevatorCages + " Elevators");
+    $("#elevator-total-price").val("$" + elevatorCost);
+    $("#final-price").val("$" + finalPrice);
+    $("#installation-fees").val("$" + elevatorCost * installationFees).toFixed(2);
+  });
+
+  //Corporate Form Calcs
+  $('#corporate-floor-number, #corporate-basement-number, #corporate-people-floor').on('keyup', function() {
+    //Commercial Form Variables
+    if ($("input[type='radio'][name='tier-selected']:checked").val() === "7565") {
+      var installationFees = 0.1
+    } else if ($("input[type='radio'][name='tier-selected']:checked").val() === "12345") {
+      var installationFees = 0.13
+    } else if ($("input[type='radio'][name='tier-selected']:checked").val() === "15400") {
+      var installationFees = 0.16
+    };
+    var corporatePeopleFloor = $("#corporate-people-floor").val();
+    var corporateFloorNumber = $("#corporate-floor-number").val();
+    var corporateBasementNumber = $("#corporate-basement-number").val();
+    //Calculations
+    var totalOccupants = corporatePeopleFloor * (Number(corporateFloorNumber) + Number(corporateBasementNumber));
+    var elevators = totalOccupants / 1000;
+    var columns = (Number(corporateFloorNumber) + Number(corporateBasementNumber)) / 20;
+    var elevatorsPerColumn = Math.ceil(elevators) / columns;
+    var totalElevators = Math.ceil(elevatorsPerColumn) * Math.ceil(columns);
+    var elevatorCost = totalElevators * $("input[type='radio'][name='tier-selected']:checked").val();
+    var finalPrice = elevatorCost + (elevatorCost * installationFees);
+    //Setting output fields
+    $("#elevator-amount").val(totalElevators + " Elevators");
+    $("#elevator-total-price").val("$" + elevatorCost);
+    $("#final-price").val("$" + finalPrice);
+    $("#installation-fees").val("$" + elevatorCost * installationFees).toFixed(2);
+  });
+
+  //Corporate Form Calcs
+  $('#hybrid-floor-number, #hybrid-basement-number, #hybrid-people-floor').on('keyup', function() {
+    //Commercial Form Variables
+    if ($("input[type='radio'][name='tier-selected']:checked").val() === "7565") {
+      var installationFees = 0.1
+    } else if ($("input[type='radio'][name='tier-selected']:checked").val() === "12345") {
+      var installationFees = 0.13
+    } else if ($("input[type='radio'][name='tier-selected']:checked").val() === "15400") {
+      var installationFees = 0.16
+    };
+    var hybridPeopleFloor = $("#hybrid-people-floor").val();
+    var hybridFloorNumber = $("#hybrid-floor-number").val();
+    var hybridBasementNumber = $("#hybrid-basement-number").val();
+    //Calculations
+    var totalOccupants = hybridPeopleFloor * (Number(hybridFloorNumber) + Number(hybridBasementNumber));
+    var elevators = totalOccupants / 1000;
+    var columns = (Number(hybridFloorNumber) + Number(hybridBasementNumber)) / 20;
+    var elevatorsPerColumn = Math.ceil(elevators) / columns;
+    var totalElevators = Math.ceil(elevatorsPerColumn) * Math.ceil(columns);
+    var elevatorCost = totalElevators * $("input[type='radio'][name='tier-selected']:checked").val();
+    var finalPrice = elevatorCost + (elevatorCost * installationFees);
+    //Setting output fields
+    $("#elevator-amount").val(totalElevators + " Elevators");
+    $("#elevator-total-price").val("$" + elevatorCost);
+    $("#final-price").val("$" + finalPrice);
+    $("#installation-fees").val("$" + elevatorCost * installationFees).toFixed(2);
+  });
 });
